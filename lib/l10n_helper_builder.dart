7e42cfdb2ac6efb78926cda9cc3680fc0aadc65b
jx9in <>
2026-03-28 20:19:33 +0800

    初次提交

---
+++ lib/l10n_helper_builder.dart
@@ -0,0 +1,9 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+import 'package:build/build.dart';
+import 'package:l10n_helper_builder/src/builder.dart';
+
+Builder l10nHelperBuilder(BuilderOptions options) => L10nHelperBuilder();
---
+++ lib/src/builder.dart
@@ -0,0 +1,194 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:analyzer/dart/analysis/utilities.dart';
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:build/build.dart';
+import 'package:collection/collection.dart';
+import 'package:dart_style/dart_style.dart';
+import 'package:path/path.dart' as p;
+import 'package:yaml/yaml.dart';
+
+class L10nConfig {
+  final String arbDir;
+  final String outputDir;
+  final String templateArbFile;
+  final String outputLocalizationFile;
+  final String outputClass;
+  final bool nullableGetter;
+  final String? header;
+  final bool format;
+
+  const L10nConfig({
+    required this.arbDir,
+    required this.outputDir,
+    required this.templateArbFile,
+    required this.outputLocalizationFile,
+    required this.outputClass,
+    required this.nullableGetter,
+    this.header,
+    required this.format,
+  });
+
+  factory L10nConfig.fromYamlString(String yamlString) {
+    final yamlMap = loadYaml(yamlString) as YamlMap;
+    return L10nConfig(
+      arbDir: yamlMap['arb-dir'] ?? 'lib/l10n',
+      outputDir: yamlMap['output-dir'] ?? 'lib/l10n',
+      templateArbFile: yamlMap['template-arb-file'] ?? 'en.arb',
+      outputLocalizationFile:
+          yamlMap['output-localization-file'] ?? 'app_localizations.dart',
+      outputClass: yamlMap['output-class'] ?? 'AppLocalizations',
+      nullableGetter: yamlMap['nullable-getter'] ?? false,
+      header: yamlMap['header'],
+      format: yamlMap['format'] ?? false,
+    );
+  }
+}
+
+class L10nHelperBuilder extends Builder {
+  @override
+  final Map<String, List<String>> buildExtensions = {
+    '.dart': ['.helper.dart'],
+  };
+
+  @override
+  FutureOr<void> build(BuildStep buildStep) async {
+    final l10n = File('l10n.yaml');
+    if (!await l10n.exists()) return;
+
+    final l10nString = await l10n.readAsString();
+    final l10nConfig = L10nConfig.fromYamlString(l10nString);
+
+    final targetPath = p.join(
+      l10nConfig.outputDir,
+      l10nConfig.outputLocalizationFile,
+    );
+    if (buildStep.inputId.path != targetPath) return;
+
+    final content = await buildStep.readAsString(buildStep.inputId);
+    final result = _genContent(
+      content,
+      fileName: l10nConfig.outputLocalizationFile,
+      className: l10nConfig.outputClass,
+      nullable: l10nConfig.nullableGetter,
+      header: l10nConfig.header,
+      format: l10nConfig.format,
+    );
+
+    if (result != null) {
+      final outputId = buildStep.inputId.changeExtension('.helper.dart');
+      await buildStep.writeAsString(outputId, result);
+    }
+  }
+
+  String? _genContent(
+    String content, {
+    required String fileName,
+    required String className,
+    bool nullable = true,
+    String? header,
+    bool format = false,
+  }) {
+    final astResult = parseString(content: content);
+    final targetClass = astResult.unit.declarations
+        .whereType<ClassDeclaration>()
+        .firstWhereOrNull((e) => e.namePart.typeName.lexeme == className);
+    if (targetClass == null) {
+      return null;
+    }
+
+    final methods = targetClass.body.members
+        .whereType<MethodDeclaration>()
+        .where((m) => m.isGetter || (!m.isGetter && !m.isSetter && !m.isStatic))
+        .toList();
+    if (methods.isEmpty) {
+      return null;
+    }
+
+    final buffer = StringBuffer();
+    buffer.writeAll([
+      ?header,
+      "import '$fileName';",
+      'extension ${className}Helper on $className{',
+      'String${nullable ? "?" : ""} getTranslation(String key,{List<Object> args=const []}){',
+      'switch(key){',
+    ]);
+    for (final method in methods) {
+      final name = method.name.lexeme;
+      buffer.write("case '$name':");
+      if (method.isGetter) {
+        buffer.write('return $name;');
+      } else {
+        final params =
+            method.parameters?.parameters.toList(growable: false) ??
+            const <FormalParameter>[];
+        final argsList = <String>[];
+        if (params.length > 1) {
+          buffer.write(
+            "assert(args.length>=${params.length},'Not enough parameters.');",
+          );
+        } else {
+          buffer.write("assert(args.isNotEmpty,'Not enough parameters.');");
+        }
+        for (int i = 0; i < params.length; i++) {
+          final p = params[i];
+          final typeName = _getParamType(p);
+          final isNamed = p.isNamed;
+          final castExpr = 'args[$i] as $typeName';
+
+          if (isNamed) {
+            final paramName = _getParamName(p);
+            argsList.add('$paramName: $castExpr');
+          } else {
+            argsList.add(castExpr);
+          }
+          buffer.write(
+            "assert(args[$i] is $typeName,'The parameter type is incorrect.');",
+          );
+        }
+        final callArgs = argsList.join(', ');
+        buffer.write('return $name($callArgs);');
+      }
+    }
+    buffer.writeAll([
+      'default:',
+      'return ${nullable ? "null" : "key"};',
+      '}',
+      '}',
+      '}',
+    ]);
+
+    if (format) {
+      final formatter = DartFormatter(
+        languageVersion: DartFormatter.latestLanguageVersion,
+      );
+      return formatter.format(buffer.toString());
+    } else {
+      return buffer.toString();
+    }
+  }
+
+  String _getParamName(FormalParameter p) {
+    final actualParam = p is DefaultFormalParameter ? p.parameter : p;
+    if (actualParam is NormalFormalParameter) {
+      return actualParam.name?.lexeme ?? '';
+    }
+    return '';
+  }
+
+  String _getParamType(FormalParameter p) {
+    final actualParam = p is DefaultFormalParameter ? p.parameter : p;
+    if (actualParam is SimpleFormalParameter) {
+      return actualParam.type?.toSource() ?? 'dynamic';
+    }
+    if (actualParam is FunctionTypedFormalParameter) {
+      return actualParam.toSource();
+    }
+    if (actualParam is FieldFormalParameter) {
+      return actualParam.type?.toSource() ?? 'dynamic';
+    }
+
+    return 'dynamic';
+  }
+}
